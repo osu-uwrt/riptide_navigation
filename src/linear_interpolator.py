53283811cd768127bff1cc4c2b1012b45db650c6
SketchMitch <sketch0306@gmail.com>
2020-06-12 18:21:25 -0400

    added edge cases

--- src/linear_interpolator.py
+++ src/linear_interpolator.py
@@ -11,18 +11,21 @@ class ExecuteTrajectory(object):
 
     def __init__(self):
         self.actionSub = rospy.Subscriber("/execute_trajectory/goal/", ExecuteTrajectoryActionGoal, self.execute_cb)
+         # make a publisher instead of returning response (rospy.publisher)
 
     def execute_cb(self, goal):
         rospy.loginfo("There are %d points in the trajectory!" % len(goal.goal.trajectory.multi_dof_joint_trajectory.points))
         
+        # array of each point as a move_it msg
         points = goal.goal.trajectory.multi_dof_joint_trajectory.points
 
+        # arrays for position, velocity, and acceleration
         p = []
         v = []
         a = []
 
-        # create a response
-        import MultiDOFJointTrajectoryPoint as response
+        # create a response (?)
+        import MultiDOFJointTrajectory as response
 
         for i in range(len(points)):
             point = points[i]
@@ -32,38 +35,50 @@ class ExecuteTrajectory(object):
             y = point.transforms.translation.y
             z = point.transforms.translation.z
 
-
-            time = points[i + 1].time_from_start.to_sec() - points[i - 1].time_from_start.to_sec()
-
             # add position to array p      
             pos = [x, y, z]
             p.append(pos)
 
             # add velocity to array v
+            times = []
             if (i == 0):
-                v.append([0, 0, 0])
-            elif (i == len(points)):
-                # I am not sure what to put at the end of velocity array
+                p2 = p[1]
+                p1 = p[0]
+                times.append(points[1].time_from_start.to_sec() - points[0].time_from_start.to_sec())
+            elif (i == len(points) - 1):
+                end = len(points) - 1
+                p2 = p[end]
+                p1 = p[end - 1]
+                times.append(points[end].time_from_start.to_sec() - points[end].time_from_start.to_sec())
             else:
                 p2 = p[i + 1]
                 p1 = p[i - 1]
-                vel = [(p2[0] - p1[0])/time, (p2[1] - p1[1])/time, (p2[2] - p1[2])/time]
-                v.append(vel)
+                time.append(points[i + 1].time_from_start.to_sec() - points[i - 1].time_from_start.to_sec())
+            time = times[i]
+            vel = [(p2[0] - p1[0])/time, (p2[1] - p1[1])/time, (p2[2] - p1[2])/time]
+            v.append(vel)
+            response[i].velocities.linear.x = vel[0]
+            response[i].velocities.linear.y = vel[1]
+            response[i].velocities.linear.z = vel[2]
 
+        for j in range(len(v)):
+            time = times[j]
             # add acceleration to array a
-            if (i = 0):
-                a.append([0, 0, 0])
-            elif (i = len(points)):
-                #I am not sure what to put at end of accel array
+            if (j == 0):
+                p2 = v[1]
+                p1 = v[0]
+            elif (j == len(v) - 1):
+                end = len(v) - 1
+                p2 = v[end]
+                p1 = v[end - 1]
             else:
-                v2 = v[i + 1]
-                v1 = v[i - 1]
-                acc = [(v2[0] - v1[0])/time, (v2[1] - v1[1])/time, (v2[2] - v1[2])/time]
-                a.append(acc)
-
-        # add the velocity and the acceleration to the response variable
-        response.velocities.linear = v
-        response.accelerations.linear = a
+                p2 = v[i + 1]
+                p1 = v[i - 1]
+            acc = [(p2[0] - p1[0])/time, (p2[1] - p1[1])/time, (p2[2] - p1[2])/time]
+            a.append(acc)
+            response[i].accelerations.linear.x = acc[0]
+            response[i].accelerations.linear.y = acc[1]
+            response[i].accelerations.linear.z = acc[2]
 
         return response
 
